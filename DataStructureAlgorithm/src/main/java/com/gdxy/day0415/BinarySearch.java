aa9547385effceb94fbf41821f25b1fa8068faf3
whoishuang00 <2994366975@qq.com>
2026-04-16 21:00:02 +0800

    0416

--- DataStructureAlgorithm/src/main/java/com/gdxy/day0415/BinarySearch.java
+++ DataStructureAlgorithm/src/main/java/com/gdxy/day0415/BinarySearch.java
@@ -53,8 +53,98 @@ public class BinarySearch {
         }
         return -1;
     }
-/*
-*
-* */
+    /*
+    *平衡板
+    * */
+    public static int binarySearchPinhen(int[] arr,int target){
+        int i=0,j=arr.length;//j只是一个边界，绝不是目标元素，绝不参与比较
+        while(1<j-i){
+            int mid=(i+j)>>>1;
+            if(target<arr[mid]){//左
+                j=mid;
+            }else {//右
+                i=mid;
+            }
+        }
+        return (arr[i]==target) ? i:-1;
+    }
 
+
+
+/*
+二分查找：Leftmost版本
+*有重复元素，查找最近的（也就是最靠左的）
+* */
+    public static int binarySearchLeftmost(int[] arr,int target){
+        int i=0,j=arr.length-1;
+        int candidate=-1;
+        while(i<=j){
+            int mid=(i+j)>>>1;
+            if(target<arr[mid]){
+                j=mid-1;
+            }else if(arr[mid]<target){
+                i=mid+1;
+            }else {
+    //            记录候选位置
+                candidate=mid;
+                j=mid-1;
+            }
+        }
+        return candidate;
+    }
+
+    /*
+    * return: 返回>=target的最靠左索引
+    *
+    * */
+
+    public static int binarySearchLeftmost1(int[] arr,int target){
+        int i=0,j=arr.length-1;
+        while(i<=j){
+            int mid=(i+j)>>>1;
+            if(target<=arr[mid]){
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+        }
+        return i;//i>=target的最靠左索引
+    }
+    /*
+    * 二分查找：Rightmost版本
+    *有重复元素，查找最靠右的元素
+    * */
+    public static int binarySearchRightmost(int[] arr,int target){
+        int i=0,j=arr.length-1;
+        int candidate=-1;
+        while(i<=j){
+            int mid=(i+j)>>>1;
+            if(target<arr[mid]){
+                j=mid-1;
+            }else if(arr[mid]<target){
+                i=mid+1;
+            }else {
+                //            记录候选位置
+                candidate=mid;
+                i=mid+1;
+            }
+        }
+        return candidate;
+    }
+    /*
+    *return: 返回<=target的最靠右索引
+    *
+    * */
+    public static int binarySearchRightmost1(int[] arr,int target){
+        int i=0,j=arr.length-1;
+        while(i<=j){
+            int mid=(i+j)>>>1;
+            if(target<arr[mid]){
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+        }
+        return i-1;//i-1小于等于target的最靠右索引
+    }
 }
